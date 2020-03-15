7a3b6402ea795f11b92fc70d2b29d5e28608378a
gandharv <gandharvkumargarg@gmail.com>
2020-03-15 19:28:54 +0530

    client

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default () => (
+  <div className='my-16 text-center text-grey text-sm'>
+    Made by{' '}
+    <a
+      className='text-black'
+      href='https://github.com/gondar00'
+      target='_blank'
+    >
+      Gandharv
+    </a>
+  </div>
+)
